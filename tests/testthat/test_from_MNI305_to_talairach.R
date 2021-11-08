311608438182d025d170fd9582291ea27dad7d15
Tim Schäfer <ts+code@rcmd.org>
2021-11-08 15:11:32 +0100

    add basic coord test

--- tests/testthat/test_from_MNI305_to_talairach.R
+++ tests/testthat/test_from_MNI305_to_talairach.R
@@ -7,4 +7,11 @@ test_that("We can transform from MNI305 to Talairach using the FreeSurfer method
     }
 
     bp = brainparc_fs(sjd, "fsaverage");
+    test_vertex_fsaverage_lh = 145029L;
+
+
+    coord_info = coord_MNI305_info(bp$surfaces$white$lh$vertices[test_vertex_fsaverage_lh, ]);
+    # coord_info$mni152 = -39.37023 -30.47030  65.31317
+    testthat::expect_true(is.vector(coord_info)); # Just run the test to check for errors atm.
 })
+
