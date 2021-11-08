b14274a5e9d7b122e89ed913bf1f4b45b6adec6a
Tim Schäfer <ts+code@rcmd.org>
2021-11-08 12:37:23 +0100

    work on MNI152 to Talairach transform

--- R/coords_to_talairach.R
+++ R/coords_to_talairach.R
@@ -37,6 +37,38 @@ coord_fssurface_to_talairach <- function(subjects_dir, subject_id, surface_coord
 #' @param vertex_coords nx3 matrix of coordinates, e.g., typically from fsaverage surface vertices.
 #'
 #' @return nx3 numerical matrix if MNI152 coords.
-coords_fsaverage_to_MNI152 <- function(vertex_coords) {
+#'
+#' @keywords internal
+coord_fsaverage_to_MNI152 <- function(vertex_coords) {
   return(freesurferformats::doapply.transform.mtx(vertex_coords, freesurferformats::mni152reg()));
 }
+
+
+#' @title Transform MNI152 coords to Talairach space using Matthew Brett's approach.
+#'
+#' @description See \code{http://brainmap.org/training/BrettTransform.html}.
+#'
+#' @param mni152_coords nx3 numerical matrix of RAS coordinates in MNI152 space.
+#'
+#' @return nx3 numerical matrix of Talairach coordinates.
+#'
+#' @note This is published under the GPL license. See \code{https://github.com/sccn/dipfit/blob/master/mni2tal_matrix.m} and \code{https://github.com/sccn/dipfit/blob/master/mni2tal.m} for a Matlab implementation of the method. All credits go to Matthew Brett.
+#'
+#' @keywords internal
+coord_MNI152_to_talairach <- function(mni152_coords) {
+  mtx_MNI152toTal_rotn  = matrix(c(1, 0, 0, 0,
+                                 0, 0.9988, 0.0500, 0,
+                                 0, -0.0500, 0.9988, 0,
+                                 0, 0, 0, 1.0000), nrow = 4, ncol = 4, byrow = TRUE);
+
+  mtx_MNI152toTal_upZ = matrix(c(0.9900, 0, 0, 0,
+                0, 0.9700, 0, 0,
+                0, 0, 0.9200, 0,
+                0, 0, 0, 1.0000), nrow = 4, ncol = 4, byrow = TRUE);
+
+  mtx_MNI152toTal_downZ = matrix(c(0.9900, 0, 0, 0,
+                0,    0.9700,         0,         0,
+                0,         0,    0.8400,         0,
+                0,         0,         0,    1.0000), nrow = 4, ncol = 4, byrow = TRUE);
+}
+
