e72c0943304a50867c3d3d992866cda5be04a4e9
Tim Schaefer <ts+code@rcmd.org>
2021-11-24 13:30:29 +0100

    fix some minor R CMD CHECK errors

--- R/read_clusterdata.R
+++ R/read_clusterdata.R
@@ -322,8 +322,6 @@ strvec2int <- function(input) { as.integer(as.factor(input)); }
 #'
 #' @param silent logical, whether to suppress console messages.
 #'
-#' @param surface character string, the surface for which to report the coordinates. Must be contained in \code{clusterinfo$brainparc$surfaces}.
-#'
 #' @return a new version of the input data.frame, with additional columns appended.
 get_cluster_location_details <- function(clusterinfo, silent = getOption("brainloc.silent", default = FALSE)) {
 
@@ -331,7 +329,7 @@ get_cluster_location_details <- function(clusterinfo, silent = getOption("brainl
         stop("Parameter 'clusterinfo' must be a clusterinfo instance.");
     }
 
-    extrema = brainloc:::cluster_extrema(clusterinfo);
+    extrema = cluster_extrema(clusterinfo);
 
     if(! silent) {
         cat(sprintf("Computing location details for cluster extrema.\n"));
@@ -391,13 +389,13 @@ test_clusters_to_annot <- function(sjd = "~/software/freesurfer/subjects", sj="f
     options("brainloc.fs_home"="~/software/freesurfer/");
     lh_an = fsbrain::subject.annot(sjd, sj, hemi="lh", atlas="aparc"); # we abuse an atlas as a cluster overlay file in this example  because it works technically. It does not make any sense, I just did not have a Matlab surfstat output file at hand.
     rh_an = fsbrain::subject.annot(sjd, sj, hemi="rh", atlas="aparc");
-    clusteroverlay = list("lh" = brainloc:::strvec2int(lh_an$label_codes), "rh" = brainloc:::strvec2int(rh_an$label_codes));
+    clusteroverlay = list("lh" = strvec2int(lh_an$label_codes), "rh" = strvec2int(rh_an$label_codes));
     thickness = fsbrain::subject.morph.native(sjd, sj, "thickness", hemi="both", split_by_hemi = TRUE);
     tmap = list("lh"=thickness$lh * 2 - 2L, "rh"=thickness$lh * 2 - 2L);  # We abuse a cortical thickness map as a t-value map. Yes, that's really ugly.
     clinfo = clusterinfo(clusteroverlay$lh, clusteroverlay$rh, tmap$lh, tmap$rh);
     #cluster_annots = clusteroverlay_to_annot(clinfo$overlay);
     #fsbrain::vis.colortable.legend(cluster_annots$lh);
-    extrema_details = brainloc:::get_cluster_location_details(clinfo);
+    extrema_details = get_cluster_location_details(clinfo);
 }
 
 
