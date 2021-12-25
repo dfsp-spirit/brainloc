59d119883565441709a2cdff2ca934724ee54b3e
Tim Schäfer <ts+code@rcmd.org>
2021-12-25 15:17:10 +0100

    add unit test for dist matrix calculation of atlas

--- tests/testthat/test-volume_regions.R
+++ tests/testthat/test-volume_regions.R
@@ -12,3 +12,23 @@ test_that("We can compute the center of mass of coordinates.", {
     expected = c(0.0, 0.0, 1.0);
     testthat::expect_equal(cm, expected);
 })
+
+
+test_that("We can compute the center of mass of all atlas regions.", {
+
+    testthat::skip_on_cran();
+
+    if(has_fs()) {
+      fsh = fs_home();
+      lutfile = file.path(fsh, "FreeSurferColorLUT.txt");
+      segfile = file.path(fsh, 'subjects', 'fsaverage', 'mri', 'aseg.mgz');
+      named_regions = name_regions(segfile, lutfile);
+      named_regions$Unknown = NULL; # Ignore an atlas region.
+      sc = segmentation_centers(segfile, named_regions);
+      regions_dist = dist(sc);
+      dm = as.matrix(regions_dist); # convert dist object to distance matrix.
+
+      testthat::expect_equal(ncol(dm), 43L);
+      testthat::expect_equal(nrow(dm), 43L);
+    }
+})
